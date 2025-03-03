00aad45fb6d211874ad1eb3c6e587a7c7098d036
jardini houssam <jardini.houssam@uit.ac.ma>
2025-03-03 09:10:25 +0000

    new expo project

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
