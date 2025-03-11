e049847ef0be9f8a610c8b12875b6490200ff375
Sohaib Bouktiba <sohaibbouktiba2004@gmail.com>
2025-03-11 00:25:15 +0000

    Add video upload functionality in Performance screen

--- App.js
+++ App.js
@@ -1,59 +1,6 @@
-// App.js
 import React, { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-
 import AppNavigator from './navigation/AppNavigator';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from './screens/HomeScreen';
-import ProgressScreen from './screens/ProgressScreen';
-import CommunityScreen from './screens/CommunityScreen';
-import SettingsScreen from './screens/SettingsScreen';
-import CameraScreen from './screens/camera';
-import Performance from './screens/Performance';
-import PerformanceComparisonScreen from './screens/PerformanceComparisonScreen';
-import { Ionicons } from 'react-native-vector-icons';
-
-const Tab = createBottomTabNavigator();
-const Stack = createStackNavigator();
-
-// Tab Navigator Component
-function TabNavigator() {
-  return (
-    <Tab.Navigator
-      screenOptions={({ route }) => ({
-        headerShown: false,
-        tabBarStyle: {
-          backgroundColor: '#1A1F23',
-          borderTopWidth: 0,
-        },
-        tabBarActiveTintColor: '#00ffc0',
-        tabBarInactiveTintColor: '#888',
-        tabBarIcon: ({ focused, color, size }) => {
-          let iconName;
-
-          if (route.name === 'Analyze') {
-            iconName = focused ? 'analytics' : 'analytics-outline';
-          } else if (route.name === 'Progress') {
-            iconName = focused ? 'bar-chart' : 'bar-chart-outline';
-          } else if (route.name === 'Community') {
-            iconName = focused ? 'people' : 'people-outline';
-          } else if (route.name === 'Settings') {
-            iconName = focused ? 'settings' : 'settings-outline';
-          }
-
-          return <Ionicons name={iconName} size={size} color={color} />;
-        },
-      })}
-    >
-      <Tab.Screen name="Analyze" component={HomeScreen} />
-      <Tab.Screen name="Progress" component={ProgressScreen} />
-      <Tab.Screen name="Community" component={CommunityScreen} />
-      <Tab.Screen name="Settings" component={SettingsScreen} />
-    </Tab.Navigator>
-  );
-}
-
 
 export default function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false); 
