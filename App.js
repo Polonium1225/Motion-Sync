342593fa8ca8d5c555b64418698ae42fe8aaabcc
AliAch04 <ali.jallousy12@gmail.com>
2025-03-23 22:12:54 +0000

    Use --legacy-peer-deps to resolve dependency conflict

--- App.js
+++ App.js
@@ -1,9 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './navigation/AppNavigator';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
 
 export default function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false); 
+  useEffect(() => {
+    GoogleSignin.configure({
+      iosClientId: "999675398759-fog61cpqtcek5jo9cfcddmqkea6nrp1e.apps.googleusercontent.com",
+      webClientId: "999675398759-l40ou2ii4adb1d54f1pjlts69nau3feo.apps.googleusercontent.com",
+      profileImageSize: 150,
+    });
+  });
+  
 
   return (
     <NavigationContainer>
